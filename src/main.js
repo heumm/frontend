82763df8ea30f68c8c171e1b8f331f56328dd3fe
heumm <hanheum97@gmail.com>
2023-02-01 22:21:13 +0900

    [update,delete] delete bootstrap, add tailwind

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import {createApp} from 'vue';
 import store from "@/scripts/store";
 import router from "@/scripts/router";
 import App from './App.vue';
-
+import './index.css';
 
 
 createApp(App).use(store).use(router).mount('#app')
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home bg-red">
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
