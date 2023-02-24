ae37bbd1c006b2213f1728cb3a235dbdaaa7f2a0
heumm <hanheum97@gmail.com>
2023-02-25 00:41:02 +0900

    [update]ui

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -8,25 +8,67 @@
      -->
   </div>
   <main>
-    <div class="flex flex-row" v-for="item in items" :key="item">
+    <div class="flex flex-row" v-for="(item,itemId) in state.items" :key="itemId">
       
-      <div class="flex flex-auto">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="m-0.5 w-5 h-5 hover:cursor-pointer" @click="items++">
+      <div class="flex flex-auto" @mouseenter="item.isItemShowing=true" @mouseleave="item.isItemShowing=false">
+        
+        <!-- <TransitionRoot
+            :show="isShowing"
+            enter="transition-opacity duration-75"
+            enter-from="opacity-0"
+            enter-to="opacity-100"
+            leave="transition-opacity duration-150"
+            leave-from="opacity-100"
+            leave-to="opacity-0"
+          > -->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="m-0.5 w-5 h-5 hover:cursor-pointer select-none" @click="addItem">
           <path fill-rule="evenodd" d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z" clip-rule="evenodd" />
         </svg>
-      
-        <TodoList @pasteClipboard="pasteClipboard"/>
+        <!-- </TransitionRoot> -->
+        
+        <TodoList/>
       </div>
       <!-- 동적으로 추가하기 기능 구현 -->
+    </div>
+    <div class="flex" @mouseenter="isShowing=true" @mouseleave="isShowing=false">
+      
+    <!-- <TransitionRoot
+            :show="isShowing"
+            enter="transition-opacity duration-75"
+            enter-from="opacity-5"
+            enter-to="opacity-100"
+            leave="transition-opacity duration-150"
+            leave-from="opacity-100"
+            leave-to="opacity-5"
+          > -->
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="m-0.5 w-5 h-5 hover:cursor-pointer select-none" @click="addItem">
+        <path fill-rule="evenodd" d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z" clip-rule="evenodd" />
+      </svg>
+    <!-- </TransitionRoot> -->
+
     </div>
   </main>
 </template>
 <script setup>
-  import {onMounted, ref} from "vue";
+  import {onMounted, reactive, ref} from "vue";
   import TodoList from "@/components/TodoList.vue";
-
+  // import { TransitionRoot } from '@headlessui/vue';
+  const itemId = ref(0);
+  const isShowing = ref(true);
   // const scrollHeight = ref(0);  //textarea 높이 초기화
-  const items = ref(1);
+  const state = reactive({
+    items: []
+  }); //20230223 todo: reactive object로 변경
+
+  const addItem = () => {
+    console.log(itemId)
+    state.items.push({
+      id: itemId.value++,
+      content: '',
+      isItemShowing: false
+    })
+  }
+
   onMounted(() => {
     // scrollHeight.value = document.querySelector("#test").scrollHeight;  //DOM 요소 렌더링 후 높이 저장
   })
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,5 @@
 <template>
-    <input class="ml-1 w-4 h-4 self-center" type="checkbox">
+    <input class="m-1 mr-2 w-4 h-4 self-center" type="checkbox">
     <!-- <textarea name="" class="outline-0 basis-full resize-none" :style="`height:${props.scrollHeight}px`" id="test" v-model="content">
     </textarea> -->
     <div class="outline-0 flex break-all whitespace-pre-wrap basis-full"  contenteditable="plaintext-only"></div>
